2f6b915870e22b8822a272db18bb9c269ce706c8
quanwei <weiquan@weideMacBook-Pro.local>
2017-04-13 17:21:51 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import co from 'co'
+
+export default function (Vue, options) {
+    Vue.directive('s-click', {
+        bind: function(el, binding, vnode) {
+            if (typeof binding.value !== 'function') return
+            el.addEventListener('click', function () {
+                el.disabled = true
+                return co.bind(vnode)(binding.value, arguments).then(function () {
+                    el.disabled = false
+                })
+            })
+        }
+    })
+}
